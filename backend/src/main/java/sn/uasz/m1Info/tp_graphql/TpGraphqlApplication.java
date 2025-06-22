a5fbdf1b83307dda0a4c861150807bef7159bc97
eadarak00 <eadarak2001@gmail.com>
2025-06-22 21:50:10 +0000

    Initialisation du projet

---
+++ backend/src/main/java/sn/uasz/m1Info/tp_graphql/TpGraphqlApplication.java
@@ -0,0 +1,13 @@
+package sn.uasz.m1Info.tp_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGraphqlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/sn/uasz/m1Info/tp_graphql/TpGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package sn.uasz.m1Info.tp_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

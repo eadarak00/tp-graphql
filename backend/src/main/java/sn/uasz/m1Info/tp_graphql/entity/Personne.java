816acdfe724d6b8f482f36985d9fb51844945198
eadarak00 <eadarak2001@gmail.com>
2025-06-22 22:07:47 +0000

    feat: creation de la classe Personne

---
+++ backend/src/main/java/sn/uasz/m1Info/tp_graphql/entity/Personne.java
@@ -0,0 +1,45 @@
+package sn.uasz.m1Info.tp_graphql.entity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Personne {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nom;
+
+    @NotBlank
+    private String prenom;
+
+    @Past(message = "La date de naissance doit être dans le passé")
+    private LocalDate dateNaissance;
+
+    @NotBlank
+    private String adresse;
+
+    @NotBlank
+    @Pattern(regexp = "^(\\+221|00221)?(7[05678])[0-9]{7}$", message = "Numéro de téléphone invalide")
+    private String telephone;
+}

950004608824877ffde0f9eb43c22b85ec092a0b
eadarak00 <eadarak2001@gmail.com>
2025-06-22 22:15:01 +0000

    feat: creation de personne service

---
+++ backend/src/main/java/sn/uasz/m1Info/service/PersonneService.java
@@ -0,0 +1,50 @@
+package sn.uasz.m1Info.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import sn.uasz.m1Info.repository.PersonneRepository;
+import sn.uasz.m1Info.tp_graphql.entity.Personne;
+
+@Service
+@RequiredArgsConstructor
+public class PersonneService {
+
+    private final PersonneRepository repository;
+    
+    public List<Personne> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Personne> getById(Long id) {
+        return repository.findById(id);
+    }
+
+    public Personne save(Personne p) {
+        return repository.save(p);
+    }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+
+    public List<Personne> rechercherParNom(String nom) {
+        return repository.findByNomContainingIgnoreCase(nom);
+    }
+
+    public Personne update(Long id, Personne updated) {
+        return repository.findById(id)
+                .map(existing -> {
+                    existing.setNom(updated.getNom());
+                    existing.setPrenom(updated.getPrenom());
+                    existing.setAdresse(updated.getAdresse());
+                    existing.setDateNaissance(updated.getDateNaissance());
+                    existing.setTelephone(updated.getTelephone());
+                    return repository.save(existing);
+                })
+                .orElse(null);
+    }
+}
